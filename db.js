1705325a997806a1fbdcc6e961a99a30b202b027
Muneeb <muneeb.k@piserve.com>
2018-11-29 22:32:12 +0530

    modifcation is db config

--- db.js
+++ db.js
@@ -9,7 +9,10 @@ module.exports = connectToDatabase = () => {
 	}
 
 	console.log('=> using new database connection');
-	return mongoose.connect(process.env.DB).then(db => {
+	//DB
+	var databaseUri ="mongodb://crazyMongo:jHH3MMRrnJ.wK5P@cluster0-shard-00-00-rqp7c.mongodb.net:27017,cluster0-shard-00-01-rqp7c.mongodb.net:27017,cluster0-shard-00-02-rqp7c.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
+	return mongoose.connect(databaseUri).then(db => {
 		isConnected = db.connections[0].readyState;
 	});
 };
